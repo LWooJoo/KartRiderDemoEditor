fed554ad248e417139d4b1a900b4ee028aad8a89
smartse0k <js940922@gmail.com>
2019-10-09 12:37:15 +0900

    상수 정의 진행중

---
+++ class/Enum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KartRiderDemoEditor
+{
+	static class Enum
+	{
+        public static const string[,] KART_TYPE = new string[,] 
+        { 
+            {"practice1", "연습카트"},
+
+            {"burst1", "버스트 C1"},
+            {"burst2", "버스트 E2"},
+            {"burst3", "버스트 G3"},
+            {"burst4", "버스트 R4"},
+            {"burst5", "버스트 Pro"},
+        };
+	}
+}
