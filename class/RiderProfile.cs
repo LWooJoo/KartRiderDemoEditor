1e8cdfe7a30d9e53ecd9d10ed4c76c1f75d1b905
smartse0k <js940922@gmail.com>
2019-10-09 13:34:41 +0900

    프로필 생성자 추가

--- class/RiderProfile.cs
+++ class/RiderProfile.cs
@@ -24,5 +24,56 @@ namespace KartRiderDemoEditor
         public int ModeClear { get; set; }
         public int UnknownValue2 { get; set; }
         public short UnknownValue3 { get; set; }
+
+        private void init()
+        {
+            nickName = "";
+            charType = "";
+            kartType = "";
+            charColor = 0;
+            unknownValue1 = 0;
+            modeClear = 0;
+            unknownValue2 = 0;
+            unknownValue3 = 0;
+        }
+
+        public RiderProfile()
+        {
+            init();
+        }
+        
+
+        public RiderProfile(bool isRandom)
+        {
+            if (isRandom)
+            {
+                Random r = new Random();
+
+                nickName = "에디터" + r.Next(1000, 9999).ToString();
+                charType = Enum.CHAR_TYPE[r.Next(0, Enum.CHAR_TYPE.GetLength(0) - 1), 0];
+                kartType = Enum.KART_TYPE[r.Next(0, Enum.KART_TYPE.GetLength(0) - 1), 0];
+                charColor = Int32.Parse(Enum.COLOR_TYPE[r.Next(0, Enum.COLOR_TYPE.GetLength(0) - 1), 0]);
+                unknownValue1 = 0;
+                modeClear = 0;
+                unknownValue2 = 0;
+                unknownValue3 = 0;
+            }
+            else
+            {
+                init();
+            }
+        }
+
+        public RiderProfile(RiderProfile copy)
+        {
+            nickName = copy.nickName;
+            charType = copy.charType;
+            kartType = copy.kartType;
+            charColor = copy.charColor;
+            unknownValue1 = copy.unknownValue1;
+            modeClear = copy.modeClear;
+            unknownValue2 = copy.unknownValue2;
+            unknownValue3 = copy.unknownValue3;
+        }
     }
 }
